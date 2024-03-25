df7f231fbaa4186869b55c03db177245de3e5ce0
mppan <mppan@iu.edu>
2024-03-25 08:32:30 -0400

    Push

---
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
@@ -0,0 +1,35 @@
+package edu.iu.habahram.DinerPancakeHouseMerge.controllers;
+
+import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
+import edu.iu.habahram.DinerPancakeHouseMerge.repository.DinerRepository;
+import edu.iu.habahram.DinerPancakeHouseMerge.repository.PancakeHouseRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/merger")
+public class MergerController {
+    DinerRepository dinerRepository;
+    PancakeHouseRepository pancakeHouseRepository;
+
+    public MergerController(DinerRepository dinerRepository, PancakeHouseRepository pancakeHouseRepository) {
+        this.dinerRepository = dinerRepository;
+        this.pancakeHouseRepository = pancakeHouseRepository;
+    }
+
+    @GetMapping
+    public List<MenuItem> get() {
+        MenuItem[] dinerMenu = dinerRepository.getTheMenu();
+        List<MenuItem> mergerMenu = new ArrayList<>(List.of(dinerMenu));
+        List<MenuItem> pancakeHouseMenu = pancakeHouseRepository.getTheMenu();
+        mergerMenu.addAll(pancakeHouseMenu);
+
+        return mergerMenu;
+    }
+}
